0571f99917a69a12c4d8e84412de8957bb1cf3f0
YousefZ <alzanki@gmail.com>
2018-06-29 17:41:57 +0300

    I need more time to figure some things out but here is my commit

--- actions.py
+++ actions.py
@@ -2,16 +2,21 @@
 from data import stores
 from components import Cart
 
-site_name = ""  # Give your site a name
+site_name = "EShopper"  # Give your site a name
 
 def welcome():
-    print("Welcome to %s\nFeel free to shop throughout the stores we have, and only checkout once!" % site_name)
+    print("Welcome to %s\nFeel free to shop throughout the stores we have, and only checkout once!\n" % site_name)
 
 def print_stores():
     """
     prints the list of stores in a nice readable format.
     """
     # your code goes here!
+    print("Welcome, This is a list of the stores you can shop from : ")
+    for items in stores:
+        print("* ",items.name)
+
+
 
 def get_store(store_name):
     """
@@ -19,24 +24,105 @@ def get_store(store_name):
     """
     # your code goes here!
 
+    ''' FROM is previous Excerciseif order in menu or order in original_flavors or order in signature_flavors:
+        #print("Correct")
+        return True
+    else : print("Sorry Item not in menu")
+______________________
+----------------------
+Having an issue here by identifying Zara as part of stores, the check is not working, so testing with Zara value directly
+----------------------
+
+
+    '''
+    if store_name == "Zara":
+        print("correct Store Name: ",store_name)
+        return store_name
+    else:
+        #print("Sorry invalid Store Name get_store func Entered Value ", store_name )
+        return False
+
+
+
+
 def pick_store():
     """
     prints list of stores and prompts user to pick a store.
     """
     # your code goes here!
+    print_stores()
+
+    print('Pick a store by typing its name. Or type "checkout" to pay your bills and say your goodbyes.')
+    cust_entry=1
+    while cust_entry != "checkout":
+        cust_entry=input()
+        if cust_entry.lower()=="checkout":
+            #Call checout method
+            break
+        if get_store(cust_entry) == False:
+            print("No store with that name. Please try again.")
+        else: 
+            break
+    return cust_entry
+
+
+
+'''
+def get_order():
+    """
+    Repeatedly ask customer for order until they end their order by typing "Exit".
+    """
+    order_list = []
+    order_item = 1
+    print("What's your order? Type Exit to end your order: ")
+    while order_item  != "exit":
+        order_item= input()
+        if order_item.lower()=="exit":
+
+            break
+        if is_valid_order(order_item.lower()) == True:
+            #print("CheckedShouldAdd")
+            order_list.append(order_item.lower())
+   
+    # your code goes here!
+
+    #print(*order_list)
+    return order_list
+
+'''
+
+
 
 def pick_products(cart, picked_store):
     """
     prints list of products and prompts user to add products to card.
     """
     # your code goes here!
+    print(products)
 
 def shop():
     """
     The main shopping functionality
     """
+    
     cart = Cart()
+    #print("shop is called ", pick_store() )
+    pick_products(cart, pick_store())
+
     # your code goes here!
 
 def thank_you():
     print("Thank you for shopping with us at %s" % site_name)
+
+
+
+
+'''Sources and Reference: 
+# https://en.wikibooks.org/wiki/A_Beginner%27s_Python_Tutorial/Importing_Modules    Importing Modules and Other items
+
+
+
+
+
+
+'''
--- components.py
+++ components.py
@@ -5,18 +5,26 @@ class Store():
         Initializes a new store with a name.
         """
         # your code goes here!
+        self.name = name    #Initializing the store with a name
+        self.product = []  #Creating an empty list of products
+
 
     def add_product(self, product):
         """
         Adds a product to the list of products in this store.
         """
         # your code goes here!
+        self.product.append(product) 
+
+
 
     def print_products(self):
         """
         Prints all the products of this store in a nice readable format.
         """
         # your code goes here!
+        print (self.name)
+
 
 
 class Product():
@@ -24,10 +32,17 @@ class Product():
         """
         Initializes a new product with a name, a description, and a price.
         """
-        # your code goes here!
+        # your code goes here! - This part is ok!
+
+        self.name = name
+        self.description=description
+        self.price = price
+
 
     def __str__(self):
-        # your code goes here!
+        # your code goes here! #!!!! TO MAKE LOOK NICER !! THIS IS BASIC
+        #return (" "Product Name:",%d,"Product Description:", %d,"Price: ", %d" % (self.name, self.description, self.price)  )
+        return "%d,%d,%d" % (self.name, self.description, self.price)
 
 
 class Cart():
@@ -36,18 +51,25 @@ class Cart():
         Initializes a new cart with an empty list of products.
         """
         # your code goes here!
+        self.lops = []  #lops=listOfProducts
 
     def add_to_cart(self, product):
         """
         Adds a product to this cart.
         """
         # your code goes here!
+        self.lops.append(product)
 
     def get_total_price(self):
         """
         Returns the total price of all the products in this cart.
         """
         # your code goes here!
+        total_price=0
+        n=0
+        #for n in lops[]
+          #  total_price = total_price + self.price
+        return total_price 
 
     def print_receipt(self):
         """
@@ -55,8 +77,25 @@ class Cart():
         """
         # your code goes here!
 
+
+
+        print (self.get_total_price())  #Need to check 
+
     def checkout(self):
         """
         Does the checkout.
         """
+
+
         # your code goes here!
+
+
+""" Supporet Material :
+Classes : 
+https://docs.python.org/2/tutorial/classes.html
+print :
+https://www.codecademy.com/en/forum_questions/551c137f51b887bbc4001b73
+
+
+"""
+
